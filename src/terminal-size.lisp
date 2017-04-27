55d7f0e8fac86c0caca52e5d60b7290580e404f1
Fernando Borretti <eudoxiahp@gmail.com>
2017-04-27 20:29:33 -0300

    Use multiple return values instead of a cons cell to reduce allocation

--- src/terminal-size.lisp
+++ src/terminal-size.lisp
@@ -2,18 +2,17 @@
 (defpackage terminal-size
   (:use :cl)
   (:export :size)
-  (:documentation "Get the sie of the terminal."))
+  (:documentation "Get the size of the terminal."))
 (in-package :terminal-size)
 
 (defmacro winsize-slot (ptr name)
   `(cffi:foreign-slot-value ,ptr '(:struct osicat-posix:winsize) ',name))
 
-(declaim (ftype (function () (cons fixnum fixnum))
+(declaim (ftype (function () (values fixnum fixnum))
                 size))
 (defun size ()
-  "Return cons of the terminal size: the car is the column count, the cdr the
-line count."
+  "Return two values: the column and line count."
   (cffi:with-foreign-object (window-size '(:struct osicat-posix:winsize))
     (osicat-posix:ioctl 0 osicat-posix:tiocgwinsz window-size)
-    (cons (winsize-slot window-size osicat-posix:col)
-          (winsize-slot window-size osicat-posix:row))))
+    (values (winsize-slot window-size osicat-posix:col)
+            (winsize-slot window-size osicat-posix:row))))
